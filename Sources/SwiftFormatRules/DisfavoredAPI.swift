eac8f2b6a134213be13f623dbe0e7c70e43b6941
Alexis Laferrière <alaferriere@apple.com>
2023-06-27 16:37:14 -0700

    Report some API calls that are not recommended

---
+++ Sources/SwiftFormatRules/DisfavoredAPI.swift
@@ -0,0 +1,42 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftFormatCore
+@_spi(RawSyntax) import SwiftSyntax
+
+public final class DisfavoredAPI: SyntaxFormatRule {
+
+  let disfavoredAPIs = [
+    // Lower level, these heuristics are less precise as there could still be relevant in some context.
+    "%",
+    // Mid-level, these have been replaced by stdlib alternatives: components(separatedBy:), etc.
+    "componentsSeparatedByString", "rangeOfString", "stringByReplacingOccurrencesOfString",
+    // High-level, there's no reason to use this.
+    "forEach"
+  ]
+
+  public override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
+    guard let name = node.calledExpression.lastToken(viewMode: .sourceAccurate)?.with(\.leadingTrivia, []).with(\.trailingTrivia, []) else {
+      return super.visit(node)
+    }
+    if disfavoredAPIs.contains(name.text) {
+      diagnose(.disfavoredAPI(name.text), on: name)
+    }
+    return super.visit(node)
+  }
+}
+
+extension Finding.Message {
+  public static func disfavoredAPI(_ name: String) -> Finding.Message {
+    "avoid using '\(name)'"
+  }
+}
